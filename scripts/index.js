eee5c92798c2d47b86b98bfb4286a642d2710342
Renat Agaev <reker.207@mail.ru>
2022-07-04 06:03:09 +0300

    Добавлен новый класс, попап адаптирован под разные устройства, добавлена возможность добавления фото на сайт

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,53 @@
+import AddRemove from "./AddRemove.js";
+
+const popupAddItem = document.querySelector('.popup_add-item')
+const popupOverlay = document.querySelector('.popup__overlay')
+const btnAddItem = document.querySelector('.title-menu__button-add')
+const mainContainer = document.querySelector('#main-content__container')
+const formAddImage = document.querySelector('.popup__form_add-Image');
+const inputLink = document.querySelector('.input-link');
+const inputModel = document.querySelector('.input-model');
+const inputDevice = document.querySelector('.input-device');
+const inputDescription = document.querySelector('.input-description');
 const btnGrid = document.querySelector('.buttons__item-grid');
 const btnNoGrid = document.querySelector('.buttons__item-nogrid');
 const mainContent = document.querySelector('.main-content-grid');
 const mainContentContainer = document.querySelectorAll('.main-content__container-grid');
 const mainContentImage = document.querySelectorAll('.main-content__image-grid');
 const mainContentTextContainer = document.querySelectorAll('.main-content__text-container');
-const popupAddItem = document.querySelector('.popup_add-item')
-const popupOverlay = document.querySelector('.popup__overlay')
-const btnAddItem = document.querySelector('.title-menu__button-add')
+
+// Меняем дизайн меню на NO GRID 
+const changeMainContentContainerOnNoGrid  = new AddRemove({selector: mainContentContainer, NewClassName: 'main-content__container-nogrid'});
+const changeMainContentImageOnNoGrid = new AddRemove({selector: mainContentImage, NewClassName: 'main-content__image-nogrid'});
+const changeMainContentTextContainerOnNoGrid = new AddRemove({selector: mainContentTextContainer, NewClassName: 'display_none'});
+const changeMainContentOnNoGrid  = new AddRemove({selector: mainContent, NewClassName: 'main-content-nogrid'});
+
+const changeOnNoGrid = () => {
+  changeMainContentContainerOnNoGrid.changeClass()
+  changeMainContentImageOnNoGrid.changeClass()
+  changeMainContentTextContainerOnNoGrid.removeClass()
+  changeMainContentOnNoGrid.addClassForAloneSelector()
+}
+
+btnNoGrid.addEventListener('click', () => {
+  changeOnNoGrid()
+})
+
+// Меняем дизайн меню на GRID 
+const changeMainContentContainerOnGrid  = new AddRemove({selector: mainContentContainer, NewClassName: 'main-content__container-grid'});
+const changeMainContentImageOnGrid = new AddRemove({selector: mainContentImage, NewClassName: 'main-content__image-grid'});
+const changeMainContentTextContainerOnGrid = new AddRemove({selector: mainContentTextContainer, NewClassName: 'display_none'});
+const changeMainContentOnGrid  = new AddRemove({selector: mainContent, NewClassName: 'main-content-grid'});
+
+const changeOnGrid = () => {
+  changeMainContentContainerOnGrid.changeClass()
+  changeMainContentImageOnGrid.changeClass()
+  changeMainContentTextContainerOnGrid.addClass()
+  changeMainContentOnGrid.addClassForAloneSelector()
+}
+btnGrid.addEventListener('click', () => {
+  changeOnGrid()
+})
 
 // Закрытие попапа на ESC
 const closePopupOnEsc = (event) => {
@@ -33,76 +74,75 @@ btnAddItem.addEventListener('click', () => {
 popupOverlay.addEventListener('click', () => {
   closePopup()
 })
-
-// Добавляем классы при нажатие на кнопку НЕ ГРИД
-const changeOnNoGridContainer = () => {
-  mainContentContainer.forEach((item) => {
-    item.className = 'main-content__container-nogrid';
-  })
-}
-const changeOnNoGridImage = () => {
-  mainContentImage.forEach((item) => {
-    item.className = 'main-content__image-nogrid';
-  })
-}
-const changeOnNoGridTextContainer = () => {
-  mainContentTextContainer.forEach((item) => {
-    item.classList.remove('display_none');
-  })
-}
-
-// Собираем все функции добавления классов НЕ ГРИД в одну
-const changeOnNoGrid = () => {
-  mainContent.className = 'main-content-nogrid';
-  changeOnNoGridContainer();
-  changeOnNoGridImage();
-  changeOnNoGridTextContainer();
-}
-
-// Добавляем классы при нажатие на кнопку ГРИД
-const changeOnGridContainer = () => {
-  mainContentContainer.forEach((item) => {
-    item.className = 'main-content__container-grid';
-  })
-}
-const changeOnGridImage = () => {
-  mainContentImage.forEach((item) => {
-    item.className = 'main-content__image-grid';
-  })
-}
-const changeOnGridTextContainer = () => {
-  mainContentTextContainer.forEach((item) => {
-  item.classList.add('display_none');
-  })
-}
-
-// Собираем все функции добавления классов ГРИД в одну
-const changeOnGrid = () => {
-  mainContent.className = 'main-content-grid';
-  changeOnGridContainer();
-  changeOnGridImage();
-  changeOnGridTextContainer();
-}
-// Замена на классы НЕ ГРИД нажатием на кнопку НЕ ГРИД
-btnNoGrid.addEventListener('click', () => {
-  changeOnNoGrid()
-})
-// Замена на классы ГРИД нажатием на кнопку ГРИД
-btnGrid.addEventListener('click', () => {
-  changeOnGrid()
-})
-
-// Функция включающая НЕ ГРИД и отключающая кнопку ГРИД при разрешении экрана менее 630px (630 включительно)
+// Функция включающая НЕ ГРИД и отключающая кнопку ГРИД при разрешении экрана менее 631px
 window.addEventListener("resize", function() {
-  if(this.innerWidth < 630) {
+  if(this.innerWidth < 631) {
     btnGrid.disabled = true;
     btnGrid.classList.add('buttons__item-grid_disabled')
     changeOnNoGrid()
-  } else if (this.innerWidth > 630){
+  } else {
     btnGrid.disabled = false;
     btnGrid.classList.remove('buttons__item-grid_disabled')
     changeOnGrid()
   }
 }, true);
 
+const ob = [
+  {link: 'https://images.unsplash.com/photo-1536896407451-6e3dd976edd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  model: "Я",
+  device: 'CANON',
+  description: 'Привет!'
+},
+  {link: 'https://images.unsplash.com/photo-1656875394347-41798ac96aa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=755&q=80',
+  model: "Мы",
+  device: 'SAMSUNG',
+  description: 'Хэй!'
+}
+]
+
 // Функции сбора данных ипунта и сабмита формы
+
+const addInputValues = (link, model, device, description) => {
+  const photoTemplate = document.querySelector('#template').content.querySelector('.main-content__container-grid').cloneNode(true);
+  const templateImage = photoTemplate.querySelector('.main-content__image-grid');
+
+  templateImage.src = link;
+  templateImage.alt = description;
+
+  const textModel = photoTemplate.querySelector('.span-model');
+  const textDevice = photoTemplate.querySelector('.span-device');
+  const textDesription = photoTemplate.querySelector('.span-description');
+  
+  textModel.textContent = model;
+  textDevice.textContent = device;
+  textDesription.textContent = description;
+
+
+  return photoTemplate
+}
+
+// Добавляем карточку на страницу
+const createImage = (link, model, device, description) => {
+  const card = addInputValues(link, model, device, description);
+  mainContainer.prepend(card)
+}
+
+
+// Собираем все нужные данные и описываем логику при сабмите формы
+const addImage = (event) => {
+  event.preventDefault();
+  const device = inputDevice.value
+  const model = inputModel.value
+  const description = inputDescription.value
+  const link = inputLink.value
+  createImage(link, model, device, description);
+  closePopup()
+  formAddImage.reset()
+}
+// ob.forEach((item) => {
+//   createImage(item.link, item.model, item.device, item.description)
+// })
+
+
+// Сабмит формы
+formAddImage.addEventListener('submit', addImage)
